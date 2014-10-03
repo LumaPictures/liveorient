d354948339d892322db8d1d09eccda83c55094dd
Chris Lyon <chrisl@luma-pictures.com>
2014-10-03 15:37:43 -0700

    initial commit

---
+++ src/main/java/com/lumapictures/liveorient/LiveOrientHook.java
@@ -0,0 +1,66 @@
+package com.lumapictures.liveorient;
+
+import com.orientechnologies.orient.core.hook.ORecordHookAbstract;
+import com.orientechnologies.orient.core.record.ORecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zeromq.ZMQ;
+
+/**
+ * Wires up OrientDB server to ZeroMQ, where inserts/updates/deletes get published.
+ * This is used by clients that want to subscribe to database changes.
+ */
+public class LiveOrientHook extends ORecordHookAbstract {
+    private static final Logger log = LoggerFactory.getLogger(LiveOrientHook.class);
+
+    private ZMQ.Context queueCtx;
+    private ZMQ.Socket queueSock;
+
+    /**
+     * Initialize ZeroMQ server, where updates will be published to.
+     */
+    private synchronized void initZMQ() {
+        if (queueSock == null) {
+            log.debug("Initializing ZeroMQ...");
+            queueCtx = ZMQ.context(1);
+            queueSock = queueCtx.socket(ZMQ.REP);
+            queueSock.bind("tcp://*:5555");
+        }
+    }
+
+    /**
+     * Publish update to ZeroMQ server.
+     * Any connected clients will receive updates.
+     *
+     * @param action char representing action that occurred: (I)nsert, (U)pdate, (D)elete.
+     * @param record the affected record.
+     */
+    private void publish(String action, ORecord<?> record) {
+        initZMQ();
+        log.info("Publishing: %s", action);
+        // TODO: enqueue!
+    }
+
+    @Override
+    public void onRecordAfterUpdate(ORecord<?> iiRecord) {
+        publish("U", iiRecord);
+        super.onRecordAfterUpdate(iiRecord);
+    }
+
+    @Override
+    public void onRecordAfterCreate(ORecord<?> iiRecord) {
+        publish("I", iiRecord);
+        super.onRecordAfterCreate(iiRecord);
+    }
+
+    @Override
+    public void onRecordAfterDelete(ORecord<?> iiRecord) {
+        publish("D", iiRecord);
+        super.onRecordAfterDelete(iiRecord);
+    }
+
+    @Override
+    public DISTRIBUTED_EXECUTION_MODE getDistributedExecutionMode() {
+        return DISTRIBUTED_EXECUTION_MODE.BOTH;
+    }
+}
---
+++ src/test/java/com/lumapictures/liveorient/LiveOrientHookTest.java
@@ -0,0 +1,11 @@
+package com.lumapictures.liveorient;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LiveOrientHookTest {
+    @Test
+    public void test_works() {
+        assertTrue(false);
+    }
+}
